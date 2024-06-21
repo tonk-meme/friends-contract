bc8633930a3d2f16abc621e4f916db2fa73a150f
alirezatabatabaeian <alireza2541@aut.ac.ir>
2024-06-21 16:02:36 +0330

    add price shows

--- tests/Router.spec.ts
+++ tests/Router.spec.ts
@@ -1,6 +1,7 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
+import { Cell, toNano, fromNano } from '@ton/core';
 import { Router } from '../wrappers/Router';
+import { User } from '../wrappers/User';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
 
@@ -18,9 +19,15 @@ describe('Router', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        router = blockchain.openContract(Router.createFromConfig({}, code));
-
         deployer = await blockchain.treasury('deployer');
+
+        router = blockchain.openContract(Router.createFromConfig({
+            protocolFeeDestination: deployer.address,
+            protocolFeePercent: 10,
+            ownerFeePercent: 5,
+            userContractCode: await compile('User'),
+            followerContractCode: await compile('Follower'),
+        }, code));
 
         const deployResult = await router.sendDeploy(deployer.getSender(), toNano('0.05'));
 
@@ -33,7 +40,12 @@ describe('Router', () => {
     });
 
     it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and router are ready to use
+        await router.sendRegisterUser(deployer.getSender());
+        const userAddress = await router.getUserAddress(deployer.address);
+        const user = blockchain.openContract(User.createFromAddress(userAddress));
+        console.log("Share Number", 1, "Price:", 0.1, "(including protocol fees, user royalty and network fees)"); // 0.1 calculation fee
+        for (let i = 1; i <= 10; i++) {
+            console.log("Share Number", i + 1, "Price:", Number(fromNano(await user.getBuyPrice(i) - await user.getBuyPrice(i - 1))) + 0.1, "(including protocol fees, user royalty and network fees)"); // 0.1 calculation fee
+        }
     });
 });
